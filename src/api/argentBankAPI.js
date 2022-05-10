6142ea64ea9c972dd8a527bfd13806a9286375ea
gwenaelle <gwenaelle.duchesne@gmail.com>
2022-05-10 15:20:33 +0200

    mock API

---
+++ src/api/argentBankAPI.js
@@ -0,0 +1,33 @@
+// A mock function to mimic making an async request for data
+export function loginMock(user) {
+    console.log(user)
+    return new Promise((resolve) => {
+        const response = {
+            status: 200,
+            message: 'User successfully logged in',
+            body: {
+                token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNmVlZGY1MjRmMTk5NDM3NTlmODlhMCIsImlhdCI6MTY1MjA5Njg3MCwiZXhwIjoxNjUyMTgzMjcwfQ.hUiUzsicQ_LEPZDrWc5ZmVmWQA61sqkfrS0UmeALzTY',
+            },
+        }
+        setTimeout(() => resolve(response), 500)
+    })
+}
+
+export function getProfileMock(token) {
+    console.log(token)
+    return new Promise((resolve) => {
+        const response = {
+            status: 200,
+            message: 'Successfully got user profile data',
+            body: {
+                email: 'tony@stark.com',
+                firstName: 'Tony',
+                lastName: 'Stark',
+                createdAt: '2022-05-01T20:30:45.950Z',
+                updatedAt: '2022-05-01T20:30:45.950Z',
+                id: '626eedf524f19943759f89a0',
+            },
+        }
+        setTimeout(() => resolve(response), 500)
+    })
+}
